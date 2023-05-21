b5a4e2ad92ca4218ed49796ba2b761a7114f7e2c
Byron Omondi <omondibyron25@gmail.com>
2023-05-21 13:48:02 +0300

    complete assingment

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,10 @@
-def parrot():
-    pass
+def parrot(argument='Squawk!'):
+    if argument:
+        print(argument)
+        return argument
+    else:
+        print("Squawk!")
+        return "Squawk!"
+
+
+    
